7521ad980865d4af09163ec82fc4f378c5e955f3
Aleksandr <aleksandr.smyshlaev@yandex.ru>
2026-03-11 18:52:05 +0300

    add code

--- clang/compiler-course/smyshlaev_a_lab1/smyshlaev_a_lab1.cpp
+++ clang/compiler-course/smyshlaev_a_lab1/smyshlaev_a_lab1.cpp
@@ -5,11 +5,64 @@
 #include "llvm/Support/raw_ostream.h"
 
 namespace {
+
+class ThrowScanner : public clang::RecursiveASTVisitor<ThrowScanner>{
+public:
+  bool found = false;
+
+  bool VisitCXXThrowExpr(clang::CXXThrowExpr *E) {
+    found = true;
+    return false;
+  }
+  bool VisitCXXNewExpr(clang::CXXNewExpr *E) {
+    found = true;
+    return false;
+  }
+  bool VisitCallExpr(clang::CallExpr *E) {
+    if (clang::FunctionDecl *Callee = E->getDirectCallee()) {
+        
+        const auto *ftp = Callee->getType()->getAs<clang::FunctionProtoType>();
+        if (ftp && ftp->getExceptionSpecType() != clang::EST_BasicNoexcept) {
+            found = true;
+            return false;
+        }
+    }
+    return true;
+  }
+
+};
+
 class SmyshlaevAVisitor final : public clang::RecursiveASTVisitor<SmyshlaevAVisitor> {
 public:
   explicit SmyshlaevAVisitor(clang::ASTContext *context) : m_context(context) {}
+  llvm::SmallPtrSet<const clang::FunctionDecl *, 16> DangerousFunctions;
   bool VisitFunctionDecl(clang::FunctionDecl *func) {
-    func->dump();
+
+    if (!func->hasBody()) return true;
+
+    const auto* ftp = func->getType()->getAs<clang::FunctionProtoType>();
+    if(!ftp) return true;
+    if(ftp->getExceptionSpecType() != clang::EST_None) return true;
+
+    ThrowScanner scanner;
+    scanner.TraverseStmt(func->getBody());
+    if (!scanner.found) {
+        clang::FunctionProtoType::ExtProtoInfo epi = ftp->getExtProtoInfo();
+        epi.ExceptionSpec.Type = clang::EST_BasicNoexcept;
+
+        clang::QualType newType = m_context->getFunctionType(
+            ftp->getReturnType(), 
+            ftp->getParamTypes(), 
+            epi
+        );
+        func->setType(newType);
+        llvm::outs() << "Функция " << func->getNameAsString() << " теперь noexcept!\n";
+    }
+    else{
+      llvm::outs() << "Функция " << func->getNameAsString() << " осталась неизмененной\n";
+    }
+    
+    
     return true;
   }
 
@@ -40,6 +93,10 @@ public:
                  const std::vector<std::string> &args) override {
     return true;
   }
+
+  clang::PluginASTAction::ActionType getActionType() override {
+    return AddBeforeMainAction;
+}
 };
 } // namespace
 
